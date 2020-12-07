ec149676dd2f322056ad74eeab92e511bd88b089
Pandagame1337 <sstarkess@yandex.ru>
2020-12-07 14:27:15 +0300

    index final

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,18 @@
 $(function() {
     //slick slider init
     $('.js-topslider').slick({
+        infinite: true,
         speed: 600,
         dots: true,
-        arrows: false
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
     });
+
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
+
 });
